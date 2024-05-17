4d2e41f8c04e89a5bc09adb0939fea05036ed3c6
brittonlaroche <britton.laroche@gmail.com>
2024-05-17 17:32:53 -0500

    Initial Version

---
+++ files/flink/createFlightVectorTable.sql
@@ -0,0 +1,15 @@
+CREATE TABLE flight_updates.flights (
+    Airline STRING,
+    Arrival STRING,
+    Arrival_Time STRING,
+    Crew_Status STRING,
+    Departure STRING,
+    Departure_Time STRING,
+    First_Officer STRING,
+    Ident STRING,
+    Pilot STRING,
+    Price FLOAT,
+    Status STRING,
+    content STRING,
+    vector ARRAY<DOUBLE>
+);
